d1ce766d1d21e27a1a09fc9e0bd1d96fc9d969d4
vildozo <vildozo@outlook.com>
2016-02-16 16:25:23 -0400

    se convirtio la variable score en una variable global

--- src/TennisGame2.java
+++ src/TennisGame2.java
@@ -3,69 +3,72 @@ public class TennisGame2 implements TennisGame
 {
     public int player1Points = 0;
     public int player2Points = 0;
+    public String score = "";
     
     public String player1LiteralScore = "";
     public String player2LiteralScore = "";
     private String player1Name;
     private String player2Name;
+    
 
     public TennisGame2(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
+        this.score = "";
     }
 
     public String getScore(){
-        String score = tiedAndScoreLessThan40();
-        score = tiedAndScoreIsGreaterOrEqualTo40(score);
+        tiedAndScoreLessThan40();
+        tiedAndScoreIsGreaterOrEqualTo40();
         
-        score = literalScorePlayer1WinningOverNone(score);
-        score = literalScorePlayer2WinningOverNone(score);
+        literalScorePlayer1WinningOverNone();
+        literalScorePlayer2WinningOverNone();
         
-        score = player1IsWinningAndScoreLessThanOrEqual40(score);
-        score = player2IsWinningAndScoreLessThanOrEqual40(score);
+        player1IsWinningAndScoreLessThanOrEqual40();
+        player2IsWinningAndScoreLessThanOrEqual40();
         
-        score = advantagePlayer1(score);
+        advantagePlayer1();
         
-        score = advantagePlayer2(score);
+        advantagePlayer2();
         
-        score = gameForPlayer1(score);
-        score = gameForPlayer2(score);
+        gameForPlayer1();
+        gameForPlayer2();
         return score;
     }
-
-	private String gameForPlayer2(String score) {
+    
+	private void gameForPlayer2() {
 		if (player2Points>=4 && player1Points>=0 && (player2Points-player1Points)>=2)
         {
             score = "Win for player2";
         }
-		return score;
+		
 	}
 
-	private String gameForPlayer1(String score) {
+	private void gameForPlayer1() {
 		if (player1Points>=4 && player2Points>=0 && (player1Points-player2Points)>=2)
         {
             score = "Win for player1";
         }
-		return score;
+		
 	}
 
-	private String advantagePlayer2(String score) {
+	private void advantagePlayer2() {
 		if (player2Points > player1Points && player1Points >= 3)
         {
             score = "Advantage player2";
         }
-		return score;
+		 
 	}
 
-	private String advantagePlayer1(String score) {
+	private void advantagePlayer1() {
 		if (player1Points > player2Points && player2Points >= 3)
         {
             score = "Advantage player1";
         }
-		return score;
+		 
 	}
 
-	private String player2IsWinningAndScoreLessThanOrEqual40(String score) {
+	private void player2IsWinningAndScoreLessThanOrEqual40() {
 		if (player2Points>player1Points && player2Points < 4)
         {
             if (player2Points==2)
@@ -78,10 +81,10 @@ public class TennisGame2 implements TennisGame
                 player1LiteralScore="Thirty";
             score = player1LiteralScore + "-" + player2LiteralScore;
         }
-		return score;
+		 
 	}
 
-	private String player1IsWinningAndScoreLessThanOrEqual40(String score) {
+	private void player1IsWinningAndScoreLessThanOrEqual40() {
 		if (player1Points>player2Points && player1Points < 4)
         {
             if (player1Points==2)
@@ -94,10 +97,10 @@ public class TennisGame2 implements TennisGame
                 player2LiteralScore="Thirty";
             score = player1LiteralScore + "-" + player2LiteralScore;
         }
-		return score;
+		 
 	}
 
-	private String literalScorePlayer2WinningOverNone(String score) {
+	private void literalScorePlayer2WinningOverNone() {
 		if (player2Points > 0 && player1Points==0)
         {
             if (player2Points==1)
@@ -110,10 +113,9 @@ public class TennisGame2 implements TennisGame
             player1LiteralScore = "Love";
             score = player1LiteralScore + "-" + player2LiteralScore;
         }
-		return score;
-	}
+ 	}
 
-	private String literalScorePlayer1WinningOverNone(String score) {
+	private void literalScorePlayer1WinningOverNone() {
 		if (player1Points > 0 && player2Points==0)
         {
             if (player1Points==1)
@@ -126,17 +128,16 @@ public class TennisGame2 implements TennisGame
             player2LiteralScore = "Love";
             score = player1LiteralScore + "-" + player2LiteralScore;
         }
-		return score;
+		 
 	}
 
-	private String tiedAndScoreIsGreaterOrEqualTo40(String score) {
+	private void tiedAndScoreIsGreaterOrEqualTo40() {
 		if (player1Points==player2Points && player1Points>=3)
             score = "Deuce";
-		return score;
+		 
 	}
 
-	private String tiedAndScoreLessThan40() {
-		String score = "";
+	private void tiedAndScoreLessThan40() {
         if (player1Points == player2Points && player1Points < 4)
         {
             if (player1Points==0)
@@ -147,7 +148,7 @@ public class TennisGame2 implements TennisGame
                 score = "Thirty";
             score += "-All";
         }
-		return score;
+		 
 	}
     
     public void SetP1Score(int number){
